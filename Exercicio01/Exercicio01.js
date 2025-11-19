8729824775171889ed91e0bf2d5b1a51b2d3425b
CLARA RODRIGUES BRAZ <46557346806@imaccommand032.local>
2025-11-19 13:20:53 -0300

    finalizando exercicio02

---
+++ Exercicio01/Exercicio01.js
@@ -0,0 +1,23 @@
+let login = prompt("Qual o seu nome? ")
+
+switch (login) {
+    case "Admin":
+        console.log("Nome correto")
+        break;
+
+    default:
+        console.log("Nome incorreto")
+        break;
+}
+
+let senha = prompt("Qual o sua senha? ")
+
+switch (senha) {
+    case "senha123":
+        console.log("Nome correto")
+        break;
+
+    default:
+        console.log("Nome incorreto")
+        break;
+}
