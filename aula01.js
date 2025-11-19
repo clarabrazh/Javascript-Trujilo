e6419828a6860671d580c980958c2bd60fb88c48
CLARA RODRIGUES BRAZ <46557346806@imaccommand032.local>
2025-11-19 10:44:03 -0300

    clara

---
+++ aula01.js
@@ -0,0 +1,47 @@
+// window.alert("apollo lindo")
+
+// window.confirm("Hoje é sexta!")
+
+// let nome = window.prompt("Qual seu nome?")
+
+// window.alert("Bem vindo(a)!" + nome)
+
+// console.log("Apresenta mensagem na tela")
+
+// let numero01 = parseInt(prompt("Digite o primeiro numero: "))
+// let numero02 = parseInt(prompt("Digite o segundo numero: "))
+
+// let soma = numero01 + numero02
+
+// console.log(soma)
+
+// let idade = parseInt(prompt("Qual é sua idade"))
+
+// if (idade >= 18) {
+//     console. log("Você é maior de idade, da-lheee")
+// }
+// else{
+//     console. log("Você é menor de idade, Baixinho")
+// }
+
+let estacoes = prompt("Digite uma estação do ano: ")
+
+switch (estacoes) {
+    case "Verão":
+        console.log("Muito calor e praia")
+        break;
+
+    case "Outono":
+        console.log("As folhas caem, no final")
+        break;
+
+    case "Inveno":
+   console.log("Está muito frio")
+        break;
+
+    case "Primavera":
+        console.log("Eu vejo flores em Você...")
+        break;
+    default:
+        break;
+}
