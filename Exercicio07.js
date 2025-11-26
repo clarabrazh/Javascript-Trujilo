b6f276269f97c75c3c79505059fff94446bc2d11
CLARA RODRIGUES BRAZ <46557346806@imaccommand032.local>
2025-11-26 11:26:56 -0300

    finalizando exercicio 05

--- Exercicio07.js
+++ Exercicio07.js
@@ -1,17 +1,21 @@
-let valorCompra = Number(prompt("Digite o valor total da compra: "));
+window.alert("Digite o valor total da sua compra em R$: ")
 
-let desconto = 0;
+let valorcompra = parseFloat(prompt("Qual o valor total da compra?"))
 
-if (valorCompra <= 100) {
+let desconto = 0
+
+if (valorcompra <= 100) {
     desconto = 0;
-} else if (valorCompra <= 200) {
-    desconto = valorCompra * 0.10;
+} else if (valorcompra <= 200) {
+    desconto = valorcompra * 0.10;
 } else {
-    desconto = valorCompra * 0.20;
+    desconto = valorcompra * 0.20;
 }
 
-let valorFinal = valorCompra - desconto;
+let valorFinal = valorcompra - desconto;
 
-alert("Valor da compra: R$ " + valorCompra.toFixed(2) +
-      "\nDesconto aplicado: R$ " + desconto.toFixed(2) +
-      "\nValor final a pagar: R$ " + valorFinal.toFixed(2));
+alert("Valor da compra: R$ " + valorcompra)
+
+alert("Desconto aplicado: R$ " + desconto)
+
+alert("Valor final a pagar: R$ " + valorFinal)
--- Exercicio08.js
+++ Exercicio08.js
@@ -1,14 +1,4 @@
-// Exercício 8: Cálculo de IMC
-// Escreva um programa que solicita ao usuário seu peso em quilogramas e sua altura em metros.
-// Em seguida, calcule o Índice de Massa Corporal (IMC) utilizando a fórmula: IMC = peso / (altura
-// * altura). Com base no resultado, exiba uma mensagem indicando a faixa de peso em que o
-// usuário se encontra, de acordo com a tabela a seguir:
-// IMC menor que 18.5: Abaixo do peso
-// IMC de 18.5 a 24.9: Peso normal
-// IMC de 25.0 a 29.9: Sobrepeso
-// IMC de 30.0 a 34.9: Obesidade grau 1
-// IMC de 35.0 a 39.9: Obesidade grau 2
-// IMC maior ou igual a 40.0: Obesidade grau 3
+
 
 let peso = parseFloat(prompt("Digite seu peso em kg:"));
 let altura = parseFloat(prompt("Digite sua altura em metros:"));
@@ -31,4 +21,3 @@ if (imc < 18.5) {
     classificacao = "Obesidade grau 3";
 }
 
-alert("Seu IMC é: " + imc.toFixed(2) + "\nClassificação: " + classificacao);
